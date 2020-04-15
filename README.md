252f9e3fb75c5610ff5dba1ffde08d95443f9e82
Kirby <kirby.forsberg@hotmail.com>
2020-04-15 01:35:44 -0400

    added timing program execution and updated README

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Fibonacci-Generator
 ---
-Simple small script that started with me wanting to make a fibonacci generator and evolved into a customized script that runs and is visualized exclusively in the command line with th thanks to [Greenstick](https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console). 
+Simple small script that started with me wanting to make a fibonacci generator and evolved into a customized script that runs and is visualized exclusively in the command line with the thanks to [Greenstick](https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console). 
 ---
-## Introdiction
+## Introduction
 
 This script generates the Fibonacci sequence up until the users' specifcation of how big it wants the file to be. 
-It also has other functionality such as whether the user wants each term numbered or an additional newline to sperate the terms in the text file
+
+It also has other functionality such as whether the user wants each term numbered or an additional newline to sperate the terms in the text file.
+
+The program produces a file `fibonacci-generator-output.txt` which defaults to the users desktop. Further refinement will be done to allow custom output location.
 
 ## Help
 
 This script is simple. Runs in native Python without any dependencies. You can just download it and run it as normal. 
 
+Be aware with larger file sizes, the program takes a while to run. Up to a few minutes if you want a 1GB file
+
+Also be aware that the progress bar will not always be 100% when it is finished. The program is designed to reach a file size less than or equal to the limit. If the closest file size without going over is below the limit, that may be reflected in the progress bar.
+ex. 99.6%, 99.9% is considered complete
+
 ## Roadmap
 
 - [ ] Rework as CLI. Replace top variables with command line arguments.
+- [ ] Allow user to determine where the output file is.
+- [x] Add time taken to complete text file.
--- fibonacci-generator.py
+++ fibonacci-generator.py
@@ -1,6 +1,10 @@
 '''
 Determine whether you want the number of the Fibonacci term listed
 '''
+import time
+from pathlib import Path
+import argparse
+import os
 numbered = False
 
 '''
@@ -14,10 +18,6 @@ Adds a space in between each iteration for easy text file reading
 '''
 newline = True
 
-import os
-import argparse
-from pathlib import Path
-
 # ap = argparse.ArgumentParser(description='Generate Fibonacci Sequence')
 # ap.add_argument("--limit",
 # 	help="max output file size in 10 to the power of limit")
@@ -30,57 +30,65 @@ from pathlib import Path
 desktop = os.path.normpath(os.path.expanduser("~/Desktop"))
 path = desktop / Path(r'fibonacci-generator-output.txt')
 
-if os.path.exists(path):
-    print('================\n OLD FILE FOUND\n----------------\n{}\n================\n'.format(path))
-    os.remove(path) 
-    print('==============\n FILE DELETED\n==============\n')
 
-f = open(path, "a")
+def main():
+    if os.path.exists(path):
+        print('================\n OLD FILE FOUND\n----------------\n{}\n================\n'.format(path))
+        os.remove(path)
+        print('==============\n FILE DELETED\n==============\n')
 
-def fib_gen():
-    a, b = 0, 1
+    f = open(path, "a")
+
+    def fib_gen():
+        a, b = 0, 1
+        while True:
+            yield a
+            a, b = b, a + b
+
+    # curteosy of https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+    def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+            printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 *
+                                                         (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print('\r%s |%s| %s%% %s' %
+              (prefix, bar, percent, suffix), end=printEnd)
+        # Print New Line on Complete
+        if iteration == total:
+            print()
+
+    fs = fib_gen()
+    next(fs)
+    print('==================\n START GENERATION\n==================\n')
+    i = 0
+
     while True:
-        yield a
-        a, b = b, a + b
+        if numbered:
+            f.write("{} TERM:\n".format(i))
+        current_size = os.stat(path).st_size
+        f.write("{}\n".format(str(next(fs))))
+        if newline:
+            f.write('\n')
+        printProgressBar(current_size, 10**limit,
+                         prefix='Progress:', suffix='Complete', length=50)
+        i -= -1
+        if os.stat(path).st_size >= 10**limit:
+            print("\n\n======================================\n   LIMIT OF {} BYTES REACHED\n--------------------------------------".format(10**limit))
+            break
+    f.close()
 
-# curteosy of https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
-
-
-fs = fib_gen()
-next(fs)
-print('==================\n START GENERATION\n==================\n')
-i = 0
-
-while True:
-    if numbered:
-        f.write("{} TERM:\n".format(i))
-    current_size = os.stat(path).st_size
-    f.write("{}\n".format(str(next(fs))))
-    if newline:
-        f.write('\n')
-    printProgressBar(current_size, 10**limit , prefix = 'Progress:', suffix = 'Complete', length = 50)
-    i -=-1
-    if os.stat(path).st_size >= 10**limit:
-        print("\n\n======================================\n   LIMIT OF {} BYTES REACHED\n======================================".format(10**limit))
-        break
-f.close()
+start_time = time.time()
+main()
+print("   %s seconds\n======================================" % (time.time() - start_time))
